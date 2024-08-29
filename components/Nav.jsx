9a944900dacf373bbca8d9cb4e6f8564f01c2e63
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-29 22:02:24 +0300

    chore: Add Nav component to app

---
+++ components/Nav.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          alt="logo"
+          src="/assets/images/logo.svg"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      {/* Mobile Navigation */}
+    </nav>
+  );
+};
+
+export default Nav;
