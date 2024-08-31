c3e8b962fc4bfcb4431a615c908d2b50e8f49c46
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-31 15:58:38 +0300

    feat: Add desktop navigation to Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,17 @@
+"use client";
+import { getProviders, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-
 const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -14,7 +24,42 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
-      {/* Mobile Navigation */}
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" className="outline_btn" onClick={signOut}>
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                className="rounded-full"
+                alt="profile"
+                width={37}
+                height={37}
+                src="/assets/images/logo.svg"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
